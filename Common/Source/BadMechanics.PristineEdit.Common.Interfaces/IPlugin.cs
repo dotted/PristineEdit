393e529ad39dcd80b0f5bec4cc7056ee5534948b
Bo Fjord Jensen <bo@bxd.dk>
2014-12-03 15:15:18 +0100

    Add initial project and structure

---
+++ Common/Source/BadMechanics.PristineEdit.Common.Interfaces/IPlugin.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPlugin.cs" company="Bad Mechanics">
+//   Copyright © 2014 Bad Mechanics. All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the IPlugin type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BadMechanics.PristineEdit.Common.Interfaces
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+        void Do();
+    }
+}
---
+++ Desktop Client/Tests/BadMechanics.PristineEdit.DesktopClient.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Class1.cs" company="Bad Mechanics">
+//   Copyright © 2014. All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the Class1 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BadMechanics.PristineEdit.DesktopClient.Tests
+{
+    public class Class1
+    {
+    }
+}
