78fbb523e07f125a2d598119b3a06abdcc6126a2
Bo Fjord Jensen <bo@bxd.dk>
2014-12-08 01:15:44 +0100

    Add encoding detection Add initial unit/integration tests for Common.Data

---
+++ Common/Tests/BadMechanics.PristineEdit.Common.Data.Tests/FileTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using BadMechanics.PristineEdit.Common.Data;
+
+namespace BadMechanics.PristineEdit.Common.Data.Tests
+{
+    using System.IO;
+    using System.Text;
+
+    using FluentAssertions;
+
+    using File = BadMechanics.PristineEdit.Common.Data.File;
+
+    public class FileTests
+    {
+        [Fact]
+        public void OpenTest()
+        {
+            //Assert.True(false, "not implemented yet");
+        }
+
+        [Fact]
+        public void DetectTextEncodingTest()
+        {
+            var testString = "testTest123";
+            var bytes = Encoding.ASCII.GetBytes(testString);
+            var stream = new MemoryStream(bytes);
+            var encoding = File.DetectTextEncoding(stream);
+            encoding.EncodingName.Should().BeEquivalentTo("us-ascii");
+        }
+    }
+}
---
+++ Common/Tests/BadMechanics.PristineEdit.Common.Data.Tests/FileTypes/PlainTextDocumentTests.cs
@@ -0,0 +1,23 @@
+﻿namespace BadMechanics.PristineEdit.Common.Data.Tests.FileTypes
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using BadMechanics.PristineEdit.Common.Data.FileTypes;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class PlainTextDocumentTests
+    {
+        [Fact]
+        public void CanInstantiate()
+        {
+            var task = new Task<Stream>(() => new MemoryStream());
+            Action a = () => new PlainTextDocument(task);
+            a.ShouldNotThrow();
+        }
+    }
+}
